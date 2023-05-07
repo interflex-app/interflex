1deb5d8490a4387cb35790af13c8ac5cb353048c
PiotrekPKP <peterpkppeter@gmail.com>
2023-05-07 12:19:44 +0200

    fix: trpc works

--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -3,10 +3,8 @@ import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 import { AppRouter } from "../server/api/root";
-import { env } from "../env.mjs";
 
 const getBaseUrl = () => {
-  console.log(process.env.VERCEL_URL);
   if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
@@ -19,7 +17,7 @@ export const api = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            env.NODE_ENV === "development" ||
+            process.env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
