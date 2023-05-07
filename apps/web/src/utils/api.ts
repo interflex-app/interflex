e333b692b1cbec5251d0200fddfbe9a225206fb3
PiotrekPKP <peterpkppeter@gmail.com>
2023-05-07 11:20:28 +0200

    fix: env vars

--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -3,6 +3,7 @@ import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 import { AppRouter } from "../server/api/root";
+import { env } from "../env.mjs";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
@@ -17,7 +18,7 @@ export const api = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
+            env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
