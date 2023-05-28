094a058623621838aff646d299c10fb16f6b39a2
PiotrekPKP <peterpkppeter@gmail.com>
2023-05-28 09:36:24 +0200

    feat: docs base

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+  logo: (
+    <span style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+      <div
+        style={{
+          border: "1px white solid",
+          backgroundColor: "white",
+          borderRadius: "9999px",
+        }}
+      >
+        <img alt="logo" src="/assets/logo-sm.svg" width="25px" />
+      </div>
 
-export default config
+      <span>Interflex</span>
+    </span>
+  ),
+  project: {
+    link: "https://interflex.dev",
+    icon: (
+      <div
+        style={{
+          border: "1px white solid",
+          backgroundColor: "white",
+          borderRadius: "9999px",
+        }}
+      >
+        <img alt="logo" src="/assets/logo-sm.svg" width="25px" />
+      </div>
+    ),
+  },
+  docsRepositoryBase: "https://github.com/interflex-app/interflex",
+  footer: {
+    text: "Copyright © Interflex.",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Interflex",
+    };
+  },
+};
+
+export default config;
